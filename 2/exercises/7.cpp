161aefc92e1a46ebb233c13d5cef457e92e217f2
Daulet Aibazov <dwlt_ybz@proton.me>
2026-03-08 13:39:00 -0500

    exercises 7 solved

---
+++ 2/exercises/7.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+int main(int argc, char *argv[]) {
+  std::cout << "enter three string values: ";
+  std::string val1, val2, val3;
+  std::cin >> val1 >> val2 >> val3;
+  if (val1 < val2 && val1 < val3) {
+    std::cout << val1;
+    if (val2 < val3) {
+      std::cout << val2 << val3;
+    } else {
+      std::cout << val3 << val2;
+    }
+  } else if (val2 < val1 && val2 < val3) {
+    std::cout << val2;
+    if (val1 < val3) {
+      std::cout << val1 << val3;
+    } else {
+      std::cout << val3 << val1;
+    }
+  } else {
+    std::cout << val3;
+    if (val1 < val2) {
+      std::cout << val1 << val2;
+    } else {
+      std::cout << val2 << val1;
+    }
+  };
+
+  return 0;
+}
