01935edb7d457a76ed1ae0ec3764047a4b4e4398
Daulet Aibazov <dwlt_ybz@proton.me>
2026-03-12 12:46:41 -0500

    drill 5 solved

---
+++ 3/drill/5.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char *argv[]) {
+  while (true) {
+    cout << "enter two integers: ";
+    double a, b;
+    cin >> a >> b;
+    if (a == b) {
+      cout << "the numbers are equal\n";
+    } else {
+      cout << "the smaller value is: " << (a < b ? a : b) << '\n';
+      cout << "the larger value is: " << (b < a ? a : b) << '\n';
+    }
+    if (abs(a - b) < 1.0 / 100.0) {
+      cout << "the numbers are almost equal\n";
+    }
+    cout << "enter '|' if you want to exit or anything else to continue: ";
+    string terminator;
+    cin >> terminator;
+    if (terminator == "|") {
+      break;
+    }
+  }
+  return 0;
+}
