6b730b69b99e8b3212ced68671fd843d11b0159b
Daulet Aibazov <dwlt_ybz@proton.me>
2026-03-12 12:31:47 -0500

    drill 3 solved

---
+++ 3/drill/3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char *argv[]) {
+  while (true) {
+    cout << "enter two integers: ";
+    int a, b;
+    cin >> a >> b;
+    if (a == b) {
+      cout << "the numbers are equal\n";
+    } else {
+      cout << "the smaller value is: " << (a < b ? a : b) << '\n';
+      cout << "the larger value is: " << (b < a ? a : b) << '\n';
+    }
+    cout << "enter '|' if you want to exit or anything else to continue: ";
+    string terminator;
+    cin >> terminator;
+    if (terminator == "|") {
+      break;
+    }
+  }
+  return 0;
+}
