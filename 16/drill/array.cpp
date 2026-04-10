72e420721d7ab61447b6d6f52b6b1a3d16170aa7
Daulet Aibazov <dwlt_ybz@proton.me>
2026-04-10 18:28:36 -0500

    array drill sovled

--- 16/drill/array.cpp
+++ 16/drill/array.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+void f(int *a, int n) {
+  int la[10];
+  int value = 1;
+  for (int *p = &la[0]; p < &la[10]; ++p) {
+    *p = value;
+    value *= 2;
+    std::cout << *p << ' ';
+  }
+  std::cout << '\n';
+  int fa[n];
+  int i = 0;
+  for (int *p = &fa[0]; p < &fa[n]; ++p) {
+    *p = a[i];
+    i++;
+    std::cout << *p << ' ';
+  }
+}
+
 int main(int argc, char *argv[]) {
   int ga[10];
   int value = 1;
@@ -5,5 +24,14 @@ int main(int argc, char *argv[]) {
     *p = value;
     value *= 2;
   }
+
+  int aa[7];
+  value = 1;
+  for (int *p = &aa[0]; p < &aa[7]; ++p) {
+    *p = value;
+    value *= 3;
+  }
+
+  f(aa, 7);
   return 0;
 }
