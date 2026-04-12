5df8f68b1ad5af3dbdb4b2f5ffc61000e26554c3
Daulet Aibazov <dwlt_ybz@proton.me>
2026-04-12 11:37:29 -0500

    drill part 1

--- 15/drill/1.cpp
+++ 15/drill/1.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <ostream>
+
+void print_array(std::ostream &os, int *a, int n) {
+  for (int i = 0; i < n; i++) {
+    os << a[i] << ' ';
+  }
+}
+
 int main(int argc, char *argv[]) {
   int *a = new int[10];
   for (int i = 0; i < 10; i++) {
     std::cout << a[i] << '\n';
   }
+  delete[] a;
+
+  int *x = new int[7]{1, 2, 3, 4, 5, 6, 7};
+  print_array(std::cout, x, 7);
   return 0;
 }
