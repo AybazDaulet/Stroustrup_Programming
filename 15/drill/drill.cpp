d9322c6d45826f5158b724d2e8a728106e2e0739
Daulet Aibazov <dwlt_ybz@proton.me>
2026-04-12 22:48:29 -0500

    drill of chapter 15 solved

--- 15/drill/drill.cpp
+++ 15/drill/drill.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <ostream>
 #include <vector>
@@ -52,5 +53,37 @@ int main(int argc, char *argv[]) {
                       110, 111, 112, 113, 114, 115, 116, 117, 118, 119};
   print_vector(std::cout, vz);
   std::cout << '\n';
+
+  // pointers
+  int var = 7;
+  int *p1 = &var;
+  std::cout << "p1 value is " << p1 << "\np1 points to " << *p1 << '\n';
+  int *p2 = new int[7]{1, 2, 4, 8, 16, 32, 64};
+  std::cout << "p2 value is" << p2 << "\np2 points to " << *p2
+            << "\nThe complete array is ";
+  print_array(std::cout, p2, 7);
+  int *p3 = p2;
+  p1 = p2;
+  std::cout << "\nthe value of p1 " << p1 << "\nthe value of p2 " << p2
+            << "\np1 points to " << *p1 << "\np2 points to " << *p2;
+  delete[] p1;
+  p1 = p2 = p3 = nullptr;
+
+  p1 = new int[10]{1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+  p2 = new int[10];
+  for (int i = 0; i < 10; i++) {
+    p2[i] = p1[i];
+  }
+  std::cout << '\n';
+  print_array(std::cout, p2, 10);
+
+  std::vector<int> v1{1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+  std::vector<int> v2;
+  for (int x : v1) {
+    v2.push_back(x);
+  }
+  std::cout << '\n';
+  print_vector(std::cout, v2);
+
   return 0;
 }
